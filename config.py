39649e1151f595222937d687bfb68ecbf657c066
ngapngap <27039619+ngapngap@users.noreply.github.com>
2025-05-29 22:51:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ PORT = 443
 TLS_DOMAIN = "cdn.cloudflare.com"
 
 USERS = {
-    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d1": "User01",
-    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d2": "User02",
-    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d3": "User03",
-    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d4": "User04",
-    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d5": "User05",
-    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6": "User06",
-    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d7": "User07",
-    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d8": "User08",
-    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d9": "User09",
-    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5da": "User10"
+    "c87ebc6c55fd2387187aca574e6ed809": "User01",
+    "7ff756f2a5ae7696ba9f2bca8eb89838": "User02",
+    "cd2e3a6cd67101797b470a289f9221f9": "User03",
+    "f88e5cdac5ba9c5f5c829a0ad70d2194": "User04",
+    "389b20dc2b3d122026d67b75aa828c5c": "User05",
+    "b3c13c6fc82699f17ab75879da689dfc": "User06",
+    "baac3bab9c3330e13f3b97eec73968f8": "User07",
+    "b8d610a2d6316db9be49a7b26243d7b8": "User08",
+    "f53be43c08e2e70a3cc93d45c4531aa7": "User09",
+    "16ca288722d4352123a6edad90888ad9": "User10"
 }
 
 MODES = {
