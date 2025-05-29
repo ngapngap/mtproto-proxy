7400e66e18277e6794c01d71e023c72a3f76f0cc
ngapngap <27039619+ngapngap@users.noreply.github.com>
2025-05-29 22:20:24 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+PORT = 443
+TLS_DOMAIN = "cdn.cloudflare.com"
+
+USERS = {
+    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d1": "User01",
+    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d2": "User02",
+    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d3": "User03",
+    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d4": "User04",
+    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d5": "User05",
+    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6": "User06",
+    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d7": "User07",
+    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d8": "User08",
+    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d9": "User09",
+    "a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5da": "User10"
+}
+
+MODES = {
+    "secure": True,
+    "tls": True
+}
